429627273aa2cfeda450b2efa9dcfbf09fbf520a
Dan Wellman <danwellman@hotmail.com>
2018-11-23 22:25:06 +0000

    App shell

---
+++ registration-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+  get currentYear(): string {
+    return new Date().getFullYear().toString();
+  }
+}
